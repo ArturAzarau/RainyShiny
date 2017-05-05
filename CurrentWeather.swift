e29990b6e6b2298b600f500128791dee12ce5a78
Artur Azarau <artur.azarau@gmail.com>
2017-05-05 15:12:18 +0300

    JSON parsing added

--- CurrentWeather.swift
+++ CurrentWeather.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import Alamofire
+import Foundation
 
 class CurrentWeather {
     
     private var _cityName: String!
     private var _date : String!
     private var _weatherType: String!
-    private var _currentTemp: Double!
+    private var _currentTemp: Int!
     
     var cityName: String {
         return _cityName ?? ""
@@ -36,18 +37,36 @@ class CurrentWeather {
         return _weatherType ?? ""
     }
     
-    var currentTemp: Double {
-        return _currentTemp ?? 0.0
+    var currentTemp: Int {
+        return _currentTemp ?? 0
     }
     
     func downloadWeatherDetails(completed: DownloadComplete) {
         let currentWeatherURL = URL(string: CURRENT_WEATHER_URL)
         Alamofire.request(currentWeatherURL!).responseJSON{ response in
-            if let json = response.result.value {
-                print(json)
+            if let json = response.result.value as? [String: AnyObject]{
+                guard let name = json["name"] as? String else { return }
+                self._cityName = name.capitalized
+                
+                guard let weather = json["weather"] as? [[String: AnyObject]],
+                        let weatherType = weather[0]["main"] as? String? else { return }
+                self._weatherType = weatherType
+                
+                guard let main = json["main"] as? [String: AnyObject],
+                    let temp = main["temp"] as? Double else { return }
+                
+                self._currentTemp = temp.kelvinToCelsius
+                
+                print(self._currentTemp, self._weatherType, self._cityName)
             } else {
                 print("download failed")
             }
         }
-        }
+    }
+}
+
+extension Double {
+    var kelvinToCelsius: Int {
+        return Int(self - 273.15)
+    }
 }
